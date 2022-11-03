c37ecc00aac63258de3fa98fd11df40f15e7a5cd
Wayne-Thornberry <way-net@outlook.com>
2022-11-03 16:53:38 +0000

    engine code moved into /code

---
+++ code/OnlineEngine/Program.cs
@@ -0,0 +1,25 @@
+﻿using Proline.ClassicOnline.EventQueue;
+using Proline.Resource;
+using System;
+using Console = Proline.Resource.Console;
+
+namespace OnlineEngine
+{
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Started Engine");
+
+            try
+            {
+                Component.InitializeComponents();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
